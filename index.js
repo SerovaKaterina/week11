db9a7e1f7a7b61163562f35659a3a1bb997e1af7
SerovaKaterina <k.serova.k@yandex.ru>
2023-05-22 01:40:40 +0300

    add changes

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const shopCartInfo1 = document.querySelector('shop__cart_info1').textContent;
-const shopCartInfo2 = document.querySelector('shop__cart_info2').textContent;
-const shopCartInfo3 = document.querySelector('shop__cart_info3').textContent;
-const shopCartInfo4 = document.querySelector('shop__cart_info4').textContent;
+let shopCartInfo1 = document.querySelector('.shop__cart_info1').textContent;
+let shopCartInfo2 = document.querySelector('.shop__cart_info2').textContent;
+let shopCartInfo3 = document.querySelector('.shop__cart_info3').textContent;
+let shopCartInfo4 = document.querySelector('.shop__cart_info4').textContent;
 
-let resultPrice =  document.getElementById('result__price');
+let resultPrice =  document.querySelector('#result__price');
 
-resultPrice.textContent =(shopCartInfo1 + shopCartInfo2 + shopCartInfo3 + shopCartInfo4);
+ resultPrice = +shopCartInfo1 + +shopCartInfo2 + +shopCartInfo3 + +shopCartInfo4;
 console.log(resultPrice);
-
 let discountPrice = resultPrice * 0.8;
+
 const newPrice = () => {
-    resultPrice.textContent = discountPrice;
+    resultPrice.textContent = Math.round(discountPrice);
 };
 

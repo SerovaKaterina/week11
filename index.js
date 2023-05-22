7c730273564644993ec338e5695b0ba01e342d6f
SerovaKaterina <k.serova.k@yandex.ru>
2023-05-22 11:43:15 +0300

    add changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const shopCartInfo1 = document.querySelector('.shop__cart_info1').textContent;
-const shopCartInfo2 = document.querySelector('.shop__cart_info2').textContent;
-const shopCartInfo3 = document.querySelector('.shop__cart_info3').textContent;
-const shopCartInfo4 = document.querySelector('.shop__cart_info4').textContent;
+let shopCartInfo1 = document.querySelector('.shop__cart_info1').textContent;
+let shopCartInfo2 = document.querySelector('.shop__cart_info2').textContent;
+let shopCartInfo3 = document.querySelector('.shop__cart_info3').textContent;
+let shopCartInfo4 = document.querySelector('.shop__cart_info4').textContent;
 const sum = +shopCartInfo1 + +shopCartInfo2 + +shopCartInfo3 + +shopCartInfo4;
 console.log(sum);
 const resultPrice = document.querySelector('.result__price');
